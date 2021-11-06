dd6dd3c737b42bee4924e0826e03e18195877c32
dashpurohit <dkpurohit123456789@gmail.com>
2021-11-06 19:37:40 +0530

    this is my first portfolio website

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots',
+  });
+
+ let hamberger = document.querySelector( '.hamberger');
+ let times = document.querySelector('.times');
+ let mobileNav= document.querySelector('.mobilenav');
+
+
+ hamberger.addEventListener('click', function(){
+   mobileNav.classList.add('open');
+
+
+ });
+
+
+
+});
+
